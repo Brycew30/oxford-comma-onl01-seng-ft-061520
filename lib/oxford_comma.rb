a59295b21b62bd049e5d0760e6d81d04a0b31692
Bryce Whittington <brycew30@gmail.com>
2020-06-17 20:09:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 array = ["red","white","blue"]
 def oxford_comma(array)
  if array.length == 2
-   array[-2] << "and"
+  array[-2] << "and"
+  array.join
  elsif array.length == 1
-  
+  array.join
+ else 
  end
  array.join(",")
 end
